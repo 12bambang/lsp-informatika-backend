1b5d0f5cbf282ad86e4963b221ddc32dfa41bb16
anwargates <102929799+anwargates@users.noreply.github.com>
2022-12-27 03:36:24 +0700

    initial migration

--- config/plugins.js
+++ config/plugins.js
@@ -1,21 +1,79 @@
 module.exports = ({ env }) => ({
-    'users-permissions': {
-      config: {
-      jwtSecret: env('JWT_SECRET'),
-      },
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET"),
     },
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
-      },
+  },
+  "strapi-plugin-populate-deep": {
+    config: {
+      defaultDepth: 3, // Default is 5
     },
-  });
+  },
+  "fuzzy-search": {
+    enabled: true,
+    config: {
+      contentTypes: [
+        // {
+        //   uid: "api::author.author",
+        //   modelName: "author",
+        //   transliterate: true,
+        //   queryConstraints: {
+        //     where: {
+        //       $and: [
+        //         {
+        //           publishedAt: { $notNull: true },
+        //         },
+        //       ],
+        //     },
+        //   },
+        //   fuzzysortOptions: {
+        //     characterLimit: 300,
+        //     threshold: -600,
+        //     limit: 10,
+        //     keys: [
+        //       {
+        //         name: "name",
+        //         weight: 100,
+        //       },
+        //       {
+        //         name: "description",
+        //         weight: -100,
+        //       },
+        //     ],
+        //   },
+        // },
+        {
+          uid: "api::apl-01.apl-01",
+          modelName: "apl-01",
+          fuzzysortOptions: {
+            characterCount: 500,
+            threshold: -500,
+            limit: 10,
+            keys: [
+              {
+                name: "namaLengkap",
+                weight: 100,
+              },
+            ],
+          },
+        },
+      ],
+    },
+  },
+
+  // uncomment before deploy
+  // upload: {
+  //   config: {
+  //     provider: 'cloudinary',
+  //     providerOptions: {
+  //       cloud_name: env('CLOUDINARY_NAME'),
+  //       api_key: env('CLOUDINARY_KEY'),
+  //       api_secret: env('CLOUDINARY_SECRET'),
+  //     },
+  //     actionOptions: {
+  //       upload: {},
+  //       delete: {},
+  //     },
+  //   },
+  // },
+});
